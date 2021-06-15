b20c65e3b804f2fc3fa5baf8da4dd8e12d7a0ee6
SahNau <aleksnn00@gmail.com>
2021-06-15 12:16:57 +0500

    add data files

---
+++ src/test/java/data/Login.java
@@ -0,0 +1,18 @@
+package data;
+
+import lombok.Value;
+
+public class Login {
+    private Login() {
+    }
+
+    @Value
+    public static class AuthInfo {
+        String login;
+        String password;
+    }
+
+    public static AuthInfo getAuthInfo() {
+        return new AuthInfo("vasya", "qwerty123");
+    }
+}
---
+++ src/test/java/data/VerificationInfo.java
@@ -0,0 +1,14 @@
+package data;
+
+import lombok.Value;
+
+public class VerificationInfo {
+    @Value
+    public static class VerificationCode {
+        String code;
+    }
+
+    public static VerificationCode getVerificationCode() {
+        return new VerificationCode("12345");
+    }
+}
